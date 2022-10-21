57041e7cbc62c9e92491fc2dd026be48a9002d03
NavSainiUofT <thenavsta@gmail.com>
2022-10-21 15:18:02 -0400

    added basic navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+import './navbar.css'
+import React,{Component} from "react";
+
+export class Navbar extends Component {
+    render(){
+        return(
+            <>
+                <div className='navigation'>
+                    <h1 className='navigation-item'>JUMP</h1>
+                    <button className='btn btn-primary navigation-item'>Home</button>
+                    <button className='btn btn-warning navigation-item'>About</button>
+                    <button className='btn btn-danger navigation-item'>Contact Us</button>
+                </div>
+            </>
+        )
+    }
+}
